d1cf8ef91cf519487f4b9fdf11bb566b6dc949c9
Dhanraj Priyadarshi <catalystwalls@gmail.com>
2021-08-23 11:30:29 +0530

    aa

--- lib/main.dart
+++ lib/main.dart
@@ -9,8 +9,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+        title: "Custom Calculator",
         home: Scaffold(
-      body: HomePage(),
-    ));
+          body: HomePage(),
+        ));
   }
 }
